6e5b9692d34d708c40c1da676a4ab7b11f52fd13
Fabio Leuthold <42573303+FLeuthold@users.noreply.github.com>
2025-10-30 14:22:44 +0100

    auf 9 zeilen reduziert
    auf 9 zeilen reduziert
    des weiteren wieder mit bool statt bitshift

--- quintenscript - Kopie - Kopie.ps1
+++ quintenscript - Kopie - Kopie.ps1
@@ -1,30 +1,18 @@
 $ton = 'b'
-$moll = $false
-$major = 0
+$moll = $true
+$sharp = 0
 $flat = 0
 
-# ASCII-basierter Index (a=0, b=1, ..., g=6)
-$index = ([byte][char]$ton) - 97
-$index = ($index * 2) % 13
+$wurst = 2*(([byte][char]$ton - 1)%7)
+$wurst -= [int]($wurst -gt 4)
+$wurst +=  $sharp - $flat + 3 * [int]$moll
+$wurst = ($wurst + 6 * [int]($wurst % 2 -ne 0)) % 12
 
-## Bitweise Grenzkorrektur: -1 wenn Bedingung erfüllt, sonst 0
-$index += (3 - $index) -shr 31
-$index += (7 - $index) -shr 31
+echo "anz kreuz: $wurst , anz b: $(12- $wurst)"
 
-#Variante mit logik statt arithmetik
-#$index -= [int]($index -gt 3)
-#$index -= [int]($index -gt 7)
 
-# Tonartverschiebung + Mollanpassung
-$index += $major - $flat + 3 * [int]$moll
 
-# Finales Mapping in den Quintenzirkel
-$index = ($index + 9) % 12
-$index = ($index + 6 * [int]($index % 2 -ne 0)) % 12
-
-# Ausgabe
-echo "Anzahl Kreuz: $index , Anzahl b: $(12 - $index)"
-
+#if($ton -eq "h"){$ton = "b"}#ausser bei englisch, dort ist es eh schon richtig :)
 
 #immer +7 halbtöne
 #c, g, d, a, e, h, fis, cis
@@ -32,5 +20,10 @@ echo "Anzahl Kreuz: $index , Anzahl b: $(12 - $index)"
 
 
 #a, f, b, es ,as, des, ges, ces
+#b, es as, des, ges, ces, fes
+
+
+#a, f, b, es ,as, des, ges, ces
 
 #b, es as, des, ges, ces, fes
+
