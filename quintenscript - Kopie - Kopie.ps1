830b4b9e1e6422f7fafb5279b2198825ec15f59b
Fabio Leuthold <42573303+FLeuthold@users.noreply.github.com>
2025-11-03 13:50:57 +0100

    mit allen 7 modi, nicht nur dur und moll

--- quintenscript - Kopie - Kopie.ps1
+++ quintenscript - Kopie - Kopie.ps1
@@ -1,15 +1,16 @@
-$ton = 'b'
-$moll = $true
+$ton = 'c'
+$mode = 0 #0 = dur, 1 = dorisch, 2 = phrygisch, 3 = lydisch, 4=mixolydisch, 5=moll, 6=lokrisch
 $sharp = 0
-$flat = 0
+$flat = 1
 
 $wurst = 2*(([byte][char]$ton - 1)%7)
 $wurst -= [int]($wurst -gt 4)
-$wurst +=  $sharp - $flat + 3 * [int]$moll
+$wurst +=  $sharp - $flat - (2 * $mode )
+$wurst += [int]($mode -gt 2)
+$wurst += 12*[int]($wurst -lt 0)
 $wurst = ($wurst + 6 * [int]($wurst % 2 -ne 0)) % 12
 
 echo "anz kreuz: $wurst , anz b: $(12- $wurst)"
-
 
 
 #if($ton -eq "h"){$ton = "b"}#ausser bei englisch, dort ist es eh schon richtig :)
@@ -26,4 +27,5 @@ echo "anz kreuz: $wurst , anz b: $(12- $wurst)"
 #a, f, b, es ,as, des, ges, ces
 
 #b, es as, des, ges, ces, fes
+
 
