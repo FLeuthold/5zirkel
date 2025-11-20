bc182b66d41566c806ca4f418a2b6d0c657ec55b
Fabio Leuthold <42573303+FLeuthold@users.noreply.github.com>
2025-11-20 23:34:36 +0100

    Refactor prompt_read functions and update prompts

--- 5zirkel-eingabekontrolle.cpp
+++ 5zirkel-eingabekontrolle.cpp
@@ -11,7 +11,7 @@ T prompt_read(const std::string& message);
 
 template <>
 char prompt_read<char>(const std::string& message) {
-    std::cout << message;
+    std::print("{}", message);
     std::string line;
     std::getline(std::cin, line);
     if (line.size() != 1) throw std::runtime_error("Expected a single character");
@@ -20,12 +20,12 @@ char prompt_read<char>(const std::string& message) {
 
 template <>
 int prompt_read<int>(const std::string& message) {
-    std::cout << message;
+    std::print("{}", message);
     std::string s;
     std::cin >> s;
     bool negativ = false;
     int acc = 0;
-    for (unsigned i = 0; i < s.size(); ++i) {
+    for (int i = 0; i < s.size(); ++i) {
         char c = s[i];
         
         if (i == 0 && c == '-') {
@@ -36,7 +36,7 @@ int prompt_read<int>(const std::string& message) {
         acc = acc * 10 + (c - '0');
     }
     if (negativ) {
-        std::cout << "negativ " << (-1 * acc) << std::endl;
+        //std::print("negativ {} \n", (-1 * acc));
         return -1 * acc;
     }
     else {
@@ -51,19 +51,19 @@ int main() {
     int mode = 0;//0 = dur/ionisch, 1 = dorisch, 2 = phrygisch, 3 = lydisch, 4=mixolydisch, 5=moll/aeolisch, 6=lokrisch
     int halb = 0;
 
-    ton = prompt_read<char>("ton:");
+    ton = prompt_read<char>("Stammton:");
     if (ton < 97 || ton >103) {
         
         std::print("ton nicht verfügbar\n");
         return 2;
     }
-    mode = prompt_read<int>("mode:");
+    mode = prompt_read<int>("mode \n 0 = dur/ionisch \n 1 = dorisch \n 2 = phrygisch \n 3 = lydisch \n 4 = mixolydisch \n 5 = moll/aeolisch \n 6 = lokrisch \n : ");
 
     if (mode < 0 || mode > 6) {
         std::print("modus nicht verfügbar\n");
         return 3;
     }
-    halb = prompt_read<int>("halb:");
+    halb = prompt_read<int>("um wieviele halbtöne erhöhen \n (vertiefen ist einfach negativ erhöhen):");
     int wurst = 2 * ((ton - 1) % 7);
     wurst -= (int)(wurst > 4);
     wurst += halb - (2 * mode);
